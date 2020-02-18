0f167dcfb5af5e92e1200aa3c93a95a6c28779cf
Kyle Haston <microfarads@gmail>
2020-02-17 20:45:39 -0800

    Removed user data from repo. Still need it! But don't want to store it in plain text on GitHub :)

--- user_data.py
+++ user_data.py
@@ -1,8 +1,8 @@
 users = [{'addr': 'server', 'sites': ['all']},
          {'addr': 'microfarads@gmail.com', 'sites': ['Yaquina Head', 'Cliffside', 'Cape Kiwanda', 'Crestwood', 'Mustang Peak', 'Creedence']},
          {'addr': 'kevohaston@gmail.com', 'sites': ['Yaquina Head', 'Cliffside', 'Cape Kiwanda', 'Crestwood', 'Mustang Peak']},
-         {'addr': 'khaston@senvainc.com', 'sites': ['Yaquina Head', 'Cliffside', 'Cape Kiwanda', 'Crestwood', 'Mustang Peak']},
-         {'addr': 'khaston@pdx.edu', 'sites': ['Mustang Peak', 'Creedence']},
+         {'addr': 'khaston@senvainc.com', 'sites': ['Yaquina Head', 'Cliffside', 'Cape Kiwanda', 'Crestwood', 'Mustang Peak', 'Creedance']},
+         # {'addr': 'khaston@pdx.edu', 'sites': ['Mustang Peak', 'Creedence']},
          {'addr': 'cliffhaston55@gmail.com', 'sites': ['Mustang Peak', 'Creedence']},
          {'addr': 'Khaston626@gmail.com', 'sites': ['Mustang Peak', 'Creedence']},
          ]
