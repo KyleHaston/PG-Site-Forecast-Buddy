19978bfb69f9e99ba11558ab926433cbe06eb78e
Kyle Haston <microfarads@gmail.com>
2020-02-17 21:13:58 -0800

    Added a site (Oceanside).

--- site_data.py
+++ site_data.py
@@ -1,3 +1,66 @@
+# Per http://cascadeparaglidingclub.org/site-guide/
+# Columbia River Gorge
+#     Bald Butte
+#     Bald Butte North
+#     Bingen
+#     Cliffside
+#     Dalles Mtn Ranch (Closed)
+#     Dog Mtn/Gorge (Closed)
+#     Doug’s Beach (Closed)
+#     Eagle Caves
+#     Hamilton Mountain (Closed)
+#
+# Willamette Valley
+#     Chehalem
+#     Gales Creek (Closed)
+#     Gobblers (Closed)
+#     Peterson Butte
+#
+# Coast Range
+#     Marys Peak
+#     Mt. Hebo (Closed)
+#     Prairie Peak (Closed)
+#
+# Coast
+#     Angora Knob  (Closed)
+#     Area B
+#     Cape Kiwanda
+#     Cape Lookout State Park
+#     Cape Perpetua (not a CPC site)
+#     Ecola State Park (closed)
+#     Neahkahnie (Closed)
+#     Oceanside
+#     Rock Creek (not a CPC site)
+#     Saddle Mtn (Closed)
+#     Sand Lake Dunes
+#     Sollie Smith/Kilchis
+#     Yaquina Head north (not a CPC site)
+#     Yaquina Head south (not a CPC site)
+#
+# Cascades
+#     Hoover Ridge
+#     Mount St. Helens (not a CPC site)
+#     Mount Hood (not a CPC site)
+#     Silver Star
+#     Toutle River Valley (Closed)
+#
+# Southern Oregon
+#     Woodrat (RVHPA site)
+#
+# South-Central Oregon
+#     Abert Rim (not a CPC site)
+#     Lakeview (not a CPC site)
+#     Winter Ridge (not a CPC site)
+#
+# Central Oregon
+#     Pine Mountain (DAR site)
+#
+# Eastern Oregon
+#     Mt. Howard (not a CPC site)
+#
+# Other sites
+#     Kiting sites (not a CPC site)
+
 sites = [{'Name': 'Yaquina Head',
                                 'lat': '44.67640', 'lon': '-124.07810', 'timezone_str': 'America/Los_Angeles',
                                 'windDirLower': 270, 'windDirUpper': 360,
@@ -20,6 +83,12 @@ sites = [{'Name': 'Yaquina Head',
                               'lat': '46.178162', 'lon': '-123.980713', 'timezone_str': 'America/Los_Angeles',
                               'windDirLower': 290, 'windDirUpper': 205,
                               'windLower': 12, 'windUpper': 14,
+                              'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+                              'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
+         {'Name': 'Oceanside',
+                              'lat': '45.4654', 'lon': '-123.9710', 'timezone_str': 'America/Los_Angeles',
+                              'windDirLower': 202, 'windDirUpper': 270,  # SSW-W
+                              'windLower': 5, 'windUpper': 15,
                               'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
                               'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
          {'Name': 'Crestwood',
