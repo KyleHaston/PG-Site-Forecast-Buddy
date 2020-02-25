36ecce51901c175833cac2271c6cef2fdd99b102
Kyle Haston <microfarads@gmail.com>
2020-02-24 17:16:16 -0800

    added some test sites

---
+++ site_data_OR_test_sites.py
@@ -0,0 +1,64 @@
+# Compass Points -------------------------------------------------------------------------------------------------------
+# Abbreviation	Middle azimuth
+# N	      0.000°
+# NNE	 22.500°
+# NE	 45.000°
+# ENE	 67.500°
+# E	     90.000°
+# ESE	112.500°
+# SE	135.000°
+# SSE	157.500°
+# S	    180.000°
+# SSW	202.500°
+# SW	225.000°
+# WSW	247.500°
+# W	    270.000°
+# WNW	292.500°
+# NW	315.000°
+# NNW	337.500°
+# N	    360.000°
+
+# OR Kiting Sites
+OR_kiting_sites = [
+    # {'Name': '',
+    #  'Desc': '',
+    #  'lat': '', 'lon': '', 'timezone_str': 'America/Los_Angeles',
+    #  'windDirLower': 0, 'windDirUpper': 360,  # TODO: Get real wind dirs
+    #  'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+    #  'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+    #  'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
+
+    {'Name': 'Hagg Lake Section D Kiting Site?',
+     'Desc': '',
+     'lat': '45.557533', 'lon': '-123.163262', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 170, 'windDirUpper': 190,  # Looks like S wind would be good. TODO: Get real wind dirs
+     'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
+
+    {'Name': 'Hagg Lake Section C Kiting Site?',
+     'Desc': '',
+     'lat': '45.491223', 'lon': '-123.214454', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 135, 'windDirUpper': 315,  # Looks like SE to NW wind would be good. TODO: Get real wind dirs
+     'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
+
+    {'Name': 'Scoggins Creek Hill Kiting Site?',
+     'Desc': '',
+     'lat': '45.455049', 'lon': '-123.177427', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 45, 'windDirUpper': 112,  # Looks like ESE to NE wind would be good. TODO: Get real wind dirs
+     'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
+
+    {'Name': 'Summit Ave Hilltop Kiting Site?',
+     'Desc': '',
+     'lat': '45.467514', 'lon': '-123.266701', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 0, 'windDirUpper': 360,  # TODO: Get real wind dirs
+     'windLower': 4, 'windUpper': 15,  # TODO: Get real wind speeds
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': False, 'show_snowLevel': False},
+
+
+]
