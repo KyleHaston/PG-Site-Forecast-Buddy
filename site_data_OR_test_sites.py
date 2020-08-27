07839b36f83b8f3873fe6e6fb4b293348d47e7c2
Kyle Haston <microfarads@gmail.com>
2020-08-26 22:02:56 -0700

    Added Tygh Ridge

--- site_data_OR_test_sites.py
+++ site_data_OR_test_sites.py
@@ -73,4 +73,35 @@ OR_test_sites = [
      'link': 'https://goo.gl/maps/B6NYS4xB8uz1ZNki8',
      'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
      'show_pop': True, 'show_qpf': True, 'show_snowAmt': True, 'show_snowLevel': True},
+
+    {'Name': 'Tygh?', 'Region': 'Tygh Ridge, OR',
+     'Info': 'Launch @ 3330\' LZ @ 2860\'',
+     # LZ: 45.305733, -121.110091
+     'lat': '45.299156', 'lon': '-121.090714', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 315, 'windDirUpper': 45,  # Guessing NW to NE possible. Need to check it out in person.
+     'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+     'link': 'https://goo.gl/maps/geH72q5sw6JKMSYM9',
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': True, 'show_snowLevel': True},
+
+    {'Name': 'Tygh West', 'Region': 'Tygh Ridge, OR',
+     'Info': 'Launch @ 3180\' LZ @ 2780\'',
+     # LZ: 45.302160, -121.122778
+     'lat': '45.295592', 'lon': '-121.112203', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 293, 'windDirUpper': 360,  # Guessing WNW to N possible. Need to check it out in person.
+     'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+     'link': 'https://goo.gl/maps/baA7rEYLL1y63QPG8',
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': True, 'show_snowLevel': True},
+
+    {'Name': 'Tygh South', 'Region': 'Tygh Ridge, OR',
+     'Info': 'Launch @ 3500\' LZ @ 2700\' Seems like wind straight from South would provide sustained ridge lift.',
+     # LZ: 45.272168, -121.105821
+     'lat': '45.291153', 'lon': '-121.114031', 'timezone_str': 'America/Los_Angeles',
+     'windDirLower': 112, 'windDirUpper': 225,  # Guessing ESE to SW possible. Need to check it out in person.
+     'windLower': 5, 'windUpper': 15,  # TODO: Get real wind speeds
+     'link': 'https://goo.gl/maps/VB9RRvayz9YZTUwYA',
+     'show_dewpoint': False, 'show_rh': False, 'show_skyCover': True,
+     'show_pop': True, 'show_qpf': True, 'show_snowAmt': True, 'show_snowLevel': True},
+
 ]
